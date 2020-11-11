e11ff19ce1c9e540590094a370b849375c8bd6ed
Brandon Charles <brandoncharles.freelance@gmail.com>
2020-11-11 18:50:45 +0200

    Revert "Logout Redirect + Basic Apply Page Layout Cleanup"
    This reverts commit 4f9543175406bc7c794ac6cac2ce8670c89ae956.

--- src/Students.js
+++ src/Students.js
@@ -7,7 +7,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core';
+import { Button, Link, makeStyles } from '@material-ui/core';
 
 function Students() {
 
