c14b0307fe59a026061748d98c06afb3f85f6554
Chris Kitenge Mbuyi <47737656+thisiscmk@users.noreply.github.com>
2020-11-13 01:02:01 +0200

    edited the style of my pages

--- src/LogQuery.js
+++ src/LogQuery.js
@@ -1,27 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-//import Container from '@material-ui/core/Container';
-import { Button, Container, TextField, Typography, makeStyles } from '@material-ui/core';
+import { Button, Container, Paper, TextField, Typography, makeStyles } from '@material-ui/core';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
-import { useForm } from "react-hook-form";
-import { useHistory } from "react-router-dom";
+//import { useForm } from "react-hook-form";
+//import { useHistory } from "react-router-dom";
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
+//import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+//import { CenterFocusStrong } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
     formControl: {
         margin: theme.spacing(1),
         minWidth: 120,
       },
+
+    
+      paper: {
+        marginTop: theme.spacing(4),
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'hidden',
+        flexDirection: 'column',
+        minHeight: 200,
+        //maxWidth: 950,
+       // align: center,
+        
+     }, 
+
+     textField: {
+        width: '25ch',
+      },
+
     
     submit: {},
     root: {
       '& > *': {
         margin: theme.spacing(1),
-        width: '25ch',
+        //width: '25ch',
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        //color: 'white',
+        height: 48,
+        padding: '0 30px',
       },
     },
   }));
@@ -38,14 +63,16 @@ function LogQuery() {
   };
     return (
        <>
-       <h1>Log a Query</h1>
+       
+       <h1 >Log a Query</h1>
        <p>Please fill in the form to log a query</p>
        <p></p>
        <p></p>
-
-       <Container component="main" maxWidth="xs">
+       
+       <Container component="main" fixed>
             <Typography component="h1" variant="h5">
           </Typography>
+          <Paper className={classes.paper}>
             <form >
                 <p>Student Number</p>
                 <div>
@@ -80,14 +107,14 @@ function LogQuery() {
                 <p>Level of study</p> 
 
                 <div>
-                <FormControl variant="outlined" className={classes.formControl}>
+                <FormControl fullWidth variant="outlined" className={classes.formControl}>
                     <InputLabel id="demo-simple-select-outlined-label">Year</InputLabel>
                         <Select
                             labelId="demo-simple-select-outlined-label"
                             id="demo-simple-select-outlined"
                             style={{ margin: 8 }}
                             //helperText="Full width!"
-                            fullWidth
+                            //fullWidth
                             margin="normal"
                             value={level}
                             onChange={handleChange}
@@ -96,49 +123,64 @@ function LogQuery() {
                     <MenuItem value="">
                         <em>None</em>
                     </MenuItem>
-                    <MenuItem value={1}>First</MenuItem>
-                    <MenuItem value={2}>Second</MenuItem>
-                    <MenuItem value={3}>Third</MenuItem>
-                    <MenuItem value={4}>BTech</MenuItem>
-                    <MenuItem value={5}>Masters</MenuItem>
+                    <MenuItem value ={0}>Higher Certificate</MenuItem>
+                    <MenuItem value ={1}>First</MenuItem>
+                    <MenuItem value = {2}>Second</MenuItem>
+                    <MenuItem value = {3}>Third</MenuItem>
+                    <MenuItem value = {4}>BTech</MenuItem>
+                    <MenuItem value = {5}>Masters</MenuItem>
                          </Select>
                 </FormControl>
                 </div>
                 <p>Residence Name</p> 
                 <div>
-                <FormControl variant="outlined" className={classes.formControl}>
+                <FormControl fullWidth variant="outlined" className={classes.formControl}>
                     <InputLabel id="demo-simple-select-outlined-label">residence</InputLabel>
                         <Select
                             labelId="demo-simple-select-outlined-label"
                             id="demo-simple-select-outlined"
                             style={{ margin: 8 }}
                             //helperText="Full width!"
-                            fullWidth
+                            //fullWidth
                             margin="normal"
                             value={residence}
                             onChange={handleChange}
                             label="Residence"
         >
                     <MenuItem value=""> <em>None</em> </MenuItem>
-                    <MenuItem value={1}>Camps Bay</MenuItem>
-                    <MenuItem value={2}>Bellville</MenuItem>
-                    <MenuItem value={3}>Table View</MenuItem>
+                    <MenuItem value ={1}>Protea Hof Tower 4</MenuItem>
+                    <MenuItem value ={2}>Sacco Residence</MenuItem>
+                    <MenuItem value ={3}>Sheriff's House Residence</MenuItem>
                          </Select>
                 </FormControl>
                 </div>
                 
                 <p>Nature of Query</p>
-                <TextareaAutosize aria-label="minimum height" rowsMin={3} placeholder="Enter text here"  fullWidth/>
+                <div>
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Multiline"
+                    fullWidth
+                    multiline
+                    rows={5}
+                    label="Enter text here"
+                    variant="outlined"
+                />
+
+                </div>           
             </form>
             <p></p>
             <p></p>
+            
             <Button
                     type="submit"
-                    fullWidth
+                    //fullWidth
                     variant="contained"
                     color="primary"
                     className={classes.submit}
                 > Submit </Button>
+            </Paper>
+
             
         </Container>
        </>
