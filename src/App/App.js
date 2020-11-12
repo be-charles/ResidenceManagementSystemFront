6b37daf1751d2cdac68c29a97fedcf69222a0d64
Brandon Charles <brandoncharles.freelance@gmail.com>
2020-11-12 16:26:10 +0200

    Added StaffQuery Route so that we can see the page...

--- src/App/App.js
+++ src/App/App.js
@@ -11,8 +11,8 @@ import Students from '../Students';
 import Login from '../Auth/Login';
 import Logout from '../Auth/Logout';
 import ExamplePage from '../ExamplePage';
-import ExamplePage2 from'../ExamplePage2';
-import StaffQuery from'../StaffQuery';
+import ExamplePage2 from '../ExamplePage2';
+import StaffQuery from '../StaffQuery';
 import LogQuery from '../LogQuery';
 import HouseRules from '../HouseRules';
 
@@ -22,18 +22,22 @@ function App() {
       <div className="App">
         <Header />
         <Switch>
-          <Route path="/" exact render={props => (<Home  />)} />
-          <Route path="/examplepage2" exact render={props => (<ExamplePage2  />)} />
-          <Route path="/examplepage" exact render={props => (<ExamplePage  />)} />
+          <Route path="/login" exact render={props => (<Login />)} />
           <Route path="/logout" exact render={props => (<Logout />)} />
-          <Route path="/login" exact render={props => (<Login  />)} />
-          <Route path="/students" exact render={props => (<Students  />)} />
-          <Route path="/apply" exact render={props => (<Apply  />)} />
-          <Route path="/myprofile" exact render={props => (<MyProfile  />)} />
-          <Route path="/residence" exact render={props => (<Residences  />)} />
-          <Route path="/residence/create" exact render={props => (<CreateResidence  />)} />
-          <Route path="/logquery" exact render={props => (<LogQuery  />)} />
-          <Route path="/houserules" exact render={props => (<HouseRules  />)} />
+          <Route path="/" exact render={props => (<Home />)} />
+          
+          <Route path="/staffquery" exact render={props => (<StaffQuery />)} />
+          <Route path="/apply" exact render={props => (<Apply />)} />
+          <Route path="/myprofile" exact render={props => (<MyProfile />)} />
+          <Route path="/logquery" exact render={props => (<LogQuery />)} />
+          <Route path="/houserules" exact render={props => (<HouseRules />)} />
+
+          {/* Example pages REMOVE WHEN PROJECT IS COMPLETE */}
+          <Route path="/examplepage2" exact render={props => (<ExamplePage2 />)} />
+          <Route path="/students" exact render={props => (<Students />)} />
+          <Route path="/examplepage" exact render={props => (<ExamplePage />)} />
+          <Route path="/residence" exact render={props => (<Residences />)} />
+          <Route path="/residence/create" exact render={props => (<CreateResidence />)} />
         </Switch>
       </div>
     </Router>
