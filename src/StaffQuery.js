7f0a797c6221867e89e8c4885c4106c83d16e426
Mzileni <48517896+Mzileni@users.noreply.github.com>
2020-11-13 13:57:23 +0200

    Update StaffQuery.js

--- src/StaffQuery.js
+++ src/StaffQuery.js
@@ -1,21 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
+import ListSubheader from '@material-ui/core/ListSubheader';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
+import Collapse from '@material-ui/core/Collapse';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
 import DraftsIcon from '@material-ui/icons/Drafts';
-import TextField from '@material-ui/core/TextField';
-import { Container, Paper } from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
+
+import PropTypes from 'prop-types';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+
+function TabPanel(props) {
+
+  const { children, value, index, ...other } = props;
+
+return (
+  <div
+    role="tabpanel"
+    hidden={value !== index}
+    id={`vertical-tabpanel-${index}`}
+    aria-labelledby={`vertical-tab-${index}`}
+    {...other}
+  >
+    {value === index && (
+      <Box p={3}>
+        <Typography>{children}</Typography>
+      </Box>
+    )}
+  </div>
+
+);
+    
+}
+
+TabPanel.propTypes = {
+children: PropTypes.node,
+index: PropTypes.any.isRequired,
+value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+return {
+  id: `vertical-tab-${index}`,
+  'aria-controls': `vertical-tabpanel-${index}`,
+};
+}
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
     maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
   },
   textField: {
     margin: 'auto',
@@ -28,73 +78,98 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     minHeight: 200,
   },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+  
 }));
-
 
 function StaffQuery() {
   const classes = useStyles();
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
+  const [open, setOpen] = React.useState(true);
+  const [value, setValue] = React.useState(0);
+  
+  const handleClick = () => {
+    setOpen(!open);
+  }
 
-  const handleListItemClick = (event, index) => {
-    setSelectedIndex(index);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+
   };
+
   return (
-    <Container>
-      <Paper className={classes.paper} elevation={3}>
-      <h1>Student Queries & Comments</h1>
-
-
-      <div className={classes.root}>
-        <List component="nav" aria-label="main mailbox folders">
-          <ListItem
-            button
-            selected={selectedIndex === 0}
-            onClick={(event) => handleListItemClick(event, 0)}
-          >
+    <div>
+    <List
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      subheader={
+        <ListSubheader component="div" id="nested-list-subheader">
+         <h1>Staff Query Management</h1>
+        </ListSubheader>
+      }
+      className={classes.root}
+    >
+      <ListItem button>
+        <ListItemIcon>
+          <SendIcon />
+        </ListItemIcon>
+        <ListItemText primary="Sent mail" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <DraftsIcon />
+        </ListItemIcon>
+        <ListItemText primary="Drafts" />
+      </ListItem>
+      <ListItem button onClick={handleClick}>
+        <ListItemIcon>
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText primary="Inbox" />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem button className={classes.nested}>
             <ListItemIcon>
-              <InboxIcon />
+              <StarBorder />
             </ListItemIcon>
-            <ListItemText primary="Inbox" />
-          </ListItem>
-          <ListItem
-            button
-            selected={selectedIndex === 1}
-            onClick={(event) => handleListItemClick(event, 1)}
-          >
-            <ListItemIcon>
-              <DraftsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Drafts" />
+            <ListItemText primary="Starred" />
           </ListItem>
         </List>
-        <Divider />
-        <List component="nav" aria-label="secondary mailbox folder">
-          <ListItem
-            button
-            selected={selectedIndex === 2}
-            onClick={(event) => handleListItemClick(event, 2)}
-          >
-            <ListItemText primary="Trash" />
-          </ListItem>
-          <ListItem
-            button
-            selected={selectedIndex === 3}
-            onClick={(event) => handleListItemClick(event, 3)}
-          >
-            <ListItemText primary="Spam" />
-          </ListItem>
-        </List>
-        <br></br><br></br>
-        <div>
-<form className={classes.root} noValidate autoComplete="off">
-<h3>Search student Information</h3>
-      <TextField id="standard-basic" label="Enter Student number" />
-    </form>
-         <br></br>
-      </div>
-      </div>
-      </Paper>
-    </Container>
+      </Collapse>
+    </List>
+<br></br><br></br>
+List of Approved Students
+    <div className={classes.root}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+      >
+        <Tab label="Student#1" {...a11yProps(0)} />
+        <Tab label="Student#2" {...a11yProps(1)} />
+        <Tab label="Student#3" {...a11yProps(2)} />
+        <Tab label="Student#4" {...a11yProps(3)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+      
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+       
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+      
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+       
+      </TabPanel>
+    </div>  
+</div>
   );
 }
 
